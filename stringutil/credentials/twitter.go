1369fcd0457c850846b14cddd45c08d4a94f2a1e
BrianCoveney <coveneybrian@gmail.com>
2018-03-18 13:07:38 +0000

    Add twitter info

---
+++ stringutil/credentials/twitter.go
@@ -0,0 +1,7 @@
+package credentials
+
+
+var ConsumerKey = "GVfEgw6AQc9T7kJtgYXTGruA3"
+var ConsumerSecret ="njhflLVqEmpt54NYFkaDL7vfBMaYbUQJ7mst3UyE36LlURsP6T"
+var AccessToken = "885340272-oidxTehfUKu1KgucgVuvuQGwnffLYjG6Os1QYj0M"
+var AccessSecret = "YUnTVJeYJfcQKw08VrPnlVGKDKCvhpQRz101sxEX9Xy8Z"
